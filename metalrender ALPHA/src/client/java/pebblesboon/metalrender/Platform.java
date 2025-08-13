b356b0850a795b5fdb7b37f9224cff201dd690f2
webblepebbles <metalrender1@gmail.com>
2025-08-13 23:27:50 +1000

    replacing metalrender ALPHA directory

--- metalrender ALPHA/src/client/java/pebblesboon/metalrender/Platform.java
+++
@@ -1,7 +0,0 @@
-package pebblesboon.metalrender;
-
-final class Platform {
-    private static final String OS = System.getProperty("os.name", "").toLowerCase();
-    static boolean isMac() { return OS.contains("mac"); }
-    private Platform() {}
-}
